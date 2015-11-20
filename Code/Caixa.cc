5e895b94f8d4034e8c50348f67aa46b41e89170e
Mmorenogomez <miguel.moreno.gomez@c6s306pc43.fib.upc.es>
2015-11-20 14:06:00 +0100

    [20] Mods to Caixa.*

--- Code/Caixa.cc
+++ Code/Caixa.cc
@@ -1,24 +1,11 @@
 #include "Caixa.hh"
 
 
-pair<bool, bool> status;
-queue<Client> waitc; //Clients esperant a Caixa
-const static int MAX_PROD_RAPID = 10;
-int sumTemps;
 
 Caixa::Caixa(){
-	status.first = status.second = false;
 	sumTemps = 0;
 }
 
 int Caixa::Consultar_Temps_Espera() const {
 	return sumTemps;
-}
-
-bool Caixa::es_Oberta() const {
-	return status.first;
-}
-
-bool Caixa::es_Rapida() const {
-	return status.second;
 }
--- Code/Caixa.hh
+++ Code/Caixa.hh
@@ -21,8 +21,7 @@ private:
       Un parell de booleans té el tipus de caixa (status.first determina si és obert o tancat, i status.second
       si és normal o ràpida). Té una cua que marca els clients que es troben a la cua de la caixa. A més, va sumant els
       temps d'espera dels clients que es troben a la cua.
-	*/
-    pair<bool, bool> status;
+    */
     queue<Client> waitc; //Clients esperant a Caixa
     const static int MAX_PROD_RAPID = 10;
     int sumTemps;
@@ -39,17 +38,6 @@ public:
 	*\post Retorna el temps d'espera de la cua de la caixa.
 	*/
 	int Consultar_Temps_Espera() const;
-	
-	/*@brief Consultora de caixa oberta
-	*\pre cert
-	*\post Retorna true si la caixa és oberta
-	*/
-	bool es_Oberta() const;
-	
-	/*@brief Consultora de caixa ràpida
-	*\pre cert
-	*\post Retorna true si la caixa és ràpida
-	*/
-	bool es_Rapida() const;
+
 };
 #endif
